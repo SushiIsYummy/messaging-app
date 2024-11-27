97de28942812c6435ff8dc08aee8ff1b4c0a6067
Alvin Lau <132179406+SushiIsYummy@users.noreply.github.com>
2024-11-27 04:11:27 -0500

    feat: configure kysely with postgres connection

---
+++ backend/src/db/database.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import pg from 'pg';
+import { Kysely, PostgresDialect } from 'kysely';
+import type { DB } from './database-types.js';
+
+const { Pool } = pg;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error(
+    'Database connection string is not set in the environment variables',
+  );
+}
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString,
+  }),
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+});
